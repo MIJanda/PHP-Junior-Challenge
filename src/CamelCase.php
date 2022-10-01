9ac6c29017b21dc845f2113834993bc45163fbaf
MIsaac4 <mokiliisaacjanda@gmail.com>
2022-10-01 17:14:15 +0300

    CamelCaseTest passing

---
+++ src/CamelCase.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+class CamelCase 
+{
+    // accounts for first word in the camelcase string
+    public int $count = 1; 
+
+    public function numberOfWords (string $camelStr): int {
+        for ($i = 0; $i < strlen($camelStr) ; $i++) {
+            if (preg_match('/[A-Z]/', $camelStr[$i])) {
+                $this->count += 1;
+            }
+        }
+
+        return $this->count;
+    }
+}
